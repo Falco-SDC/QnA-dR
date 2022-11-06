9b4a287a3f2084b7a83c1d6c298bdd296acce731
admin <shethinksnyc@protonmail.com>
2022-11-05 21:32:56 -0400

    finsished routes, models, working endpoints

--- db/driver.js
+++ db/driver.js
@@ -1,6 +1,4 @@
 const mysql = require("mysql2");
-// const path = require("path");
-
 // only way to access enironment variables --due to Node?
 require("dotenv").config( { path:'../.env' } );
 
@@ -10,8 +8,6 @@ const pool = mysql.createPool({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
-
-
 
 SelectAllQuestions = () => {
   return new Promise((resolve, reject)=>{
@@ -24,13 +20,94 @@ SelectAllQuestions = () => {
   })
 }
 
+SelectAllAnswers = () =>{
+  return new Promise((resolve,reject) => {
+    pool.query('SELECT * FROM answers LIMIT 10', (error, answers)=>{
+      if(error) {
+        return reject(error)
+      }
+      return resolve(answers)
+    })
+  })
+}
+
+// this query data does not need to return data from db ?
+AddAQuestionModel = () =>{
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM questions LIMIT 20', (error, answers) => {
+      if(error) {
+        return reject(error)
+      }
+      return resolve(answers)
+    })
+  })
+}
+
+// this query promise does not need to return data?
+AddAnAnswerModal = ()=> {
+  return new Promise((resolve, reject) =>{
+    pool.query('SELECT * FROM answers LIMIT 20', (error, answers) => {
+      if(error) {
+        return reject(error)
+      }
+      return resolve(answers)
+    })
+  })
+}
+
+// Updates a question to show it was found helpful.
+UpdateAnswerHelpful = () =>{
+  return new Promise((resolve, reject) => {
+    pool.query(`SELECT * FROM answers LIMIT 9`, (error, answers) => {
+      if(error) {
+        return reject(error)
+      }
+      return resolve(answers)
+    })
+  })
+}
+
+// Updates a question to show it was reported. Note, this action does not delete the question, but the question will not be returned in the above GET request.
+UpdateReportQuestion = () =>{
+  return new Promise((resolve, reject) => {
+    pool.query(`SELECT * FROM questions LIMIT 9`, (error, questions) => {
+      if(error) {
+        return reject(error)
+      }
+      return resolve(questions)
+    })
+  })
+}
+
+UpdateAnswerHelpful = () =>{
+  return new Promise((resolve, reject) => {
+    pool.query(`SELECT * FROM questions LIMIT 9`, (error, questions) => {
+      if(error) {
+        return reject(error)
+      }
+      return resolve(questions)
+    })
+  })
+}
+
+UpdateAnswerReport = () =>{
+  return new Promise((resolve, reject) => {
+    pool.query(`SELECT * FROM questions LIMIT 9`, (error, questions) => {
+      if(error) {
+        return reject(error)
+      }
+      return resolve(questions)
+    })
+  })
+}
 
 
-
+// organize order to match controllers -- api -learn
+  // is there anywhere else i can write the queries (save for refactor)
 
 
 // 3. create promise function for getting questions
 // 4. build query with controller info
 // 5. run/return query
 
-module.exports = {SelectAllQuestions}
+module.exports = {SelectAllQuestions, SelectAllAnswers ,AddAQuestionModel, AddAnAnswerModal, UpdateAnswerHelpful,UpdateReportQuestion}
--- server/controllers.js
+++ server/controllers.js
@@ -4,56 +4,116 @@ const db = require('../db/driver.js')
    // 2. invoke model function (promise)
    //6. then respond to client
 
-
   const getAllQuestions = (req, res) => {
+    // console.log(req.query)
     let product_id = req.query
-    console.log(product_id, 'this works')
-    db.SelectAllQuestions()
+    db.SelectAllQuestions(product_id)
     .then((result) => {
       res.send(result)
     })
     .catch((error)=>{
-      console.log(error, ' send help')
+      res.sendStatus(204).send(error,'no question found')
     })
   }
 
+// how to handle if req does not have query params?
   const getAllAnswers = (req, res) => {
-
+    let page = req.query.page
+    let count = req.query.count
+    let question_id = req.params.question_id
+    db.SelectAllAnswers(question_id)
+    .then((result)=>{
+      res.send(result)
+    })
+    .catch((error)=>{
+      res.sendStatus(204).send('question not found')
+    })
   }
 
+// need to account for optional params
  const addAQuestion = (req, res) => {
-
+    let addQuestionBody = req.body.body
+    // console.log(req.body.body)
+    let addQuestionName = req.body.name
+    let addQuestionEmail = req.body.email
+    let addAQuestionProduct_id = req.body.product_id
+    db.AddAQuestionModel(addQuestionBody, addQuestionName,addQuestionEmail,addAQuestionProduct_id)
+    .then((result)=>{
+      res.sendStatus(201).send('CREATED')
+    })
+    .catch((error)=>{
+      // set headers error 'ERR_HTTP_HEADERS_SENT' -- fix response with proper error message
+      res.status(404)
+    })
  }
 
  const addAnAnswer = (req, res) => {
-
+  let question_id = req.params.question_id
+  let addAnswerBody = req.body.body
+  let addAnswerName = req.body.name
+  let addAnswerEmail = req.body.email
+  let addAnswerPhotos = req.body.photos
+  db.AddAnAnswerModal(question_id, addAnswerBody, addAnswerName, addAnswerEmail, addAnswerPhotos)
+  .then((result) => {
+    res.sendStatus(201).send('CREATED')
+  })
+  .catch((error)=>{
+    res.status(500, 'nah')
+  })
  }
 
+ // Updates a question to show it was found helpful.
  const updateHelpfulQuestion = (req, res) => {
-
+  let question_id = req.params.question_id
+  db.UpdateAnswerHelpful(question_id)
+  .then((result) => {
+    res.sendStatus(204,'NO CONTENT')
+  })
+  .catch((error)=> {
+    res.status(500,'nope')
+  })
  }
 
+//  Updates a question to show it was reported. Note, this action does not delete the question, but the question will not be returned in the above GET request.
  const updateReportQuestion = (req, res) => {
-
+  let product_id = req.params.product_id
+  db.UpdateReportQuestion(product_id)
+  .then((result)=>{
+    res.sendStatus(204, 'NO CONTENT')
+  })
+  .catch((error)=>{
+    res.status(500,'nay')
+  })
  }
 
+//  Updates an answer to show it was found helpful.
  const updateHelpfulAnswer = (req, resp) => {
-
+  let answer_id = req.params.answer_id
+  db.UpdateAnswerHelpful(answer_id)
+  .then((result)=>{
+   res.sendStatus(204)
+  })
+  .catch((error)=>{
+    res.status(500,'no in spanish')
+  })
  }
 
-  //  const addAQuestion = (req, res) => {
-  //   console.log(req.body,' tetsing')
-  //   let body = req.body.body
-  //   console.log(body)
-  //   res.sendStatus(201).send(body, 'please work ')
-  //  }
-
-
+// Updates an answer to show it has been reported
+ const updateReportAnswer = (req, resp) => {
+ let answer_id = req.params.answer_id
+ db.UpdateAnswerReport(answer_id)
+ .then((result) => {
+  res.sendStatus(204)
+ })
+ .catch((error)=> {
+  res.status(500,'nada')
+ })
+}
 
 
 
 module.exports = { getAllQuestions , getAllAnswers, addAQuestion, addAnAnswer,
-  updateHelpfulQuestion, updateReportQuestion, updateHelpfulAnswer
+  updateHelpfulQuestion, updateReportQuestion, updateHelpfulAnswer, updateReportAnswer
 
 };
 
