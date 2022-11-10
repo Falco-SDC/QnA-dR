475b3895f7c7fd4fb5494fad162c7c9818e7246f
admin <shethinksnyc@protonmail.com>
2022-11-10 15:17:00 -0500

    put and post/ updates and reports, refactored some models to eliminate redundancy with promise syntax, tested routes and db

--- db/driver.js
+++ db/driver.js
@@ -10,7 +10,6 @@ const pool = mysql.createPool({
 });
 
 SelectAllQuestions = (product_id) => {
-  console.log(product_id,'from model')
   return new Promise((resolve, reject)=>{
    // select all questions using product id
     pool.query(`SELECT questions.id as question_id ,
@@ -43,7 +42,7 @@ SelectAllAnswers = (question_id, count, page) =>{
   return new Promise((resolve,reject) => {
     pool.query(`SELECT answers.id as answer_id,
                 answers.body,
-                 answers.date_written,
+                 (SELECT FROM_UNIXTIME(answers.date_written/1000)) as date,
                  answers.answerer_name,
                 answers.helpful,
                 JSON_ARRAYAGG(
@@ -73,24 +72,10 @@ AddAQuestionModel = (setUp) =>{
               })
             }
 
-// one function for posting answer
-  // chain query to next query
-
-  // Promise.all([
-  //   pool.query(`INSERT INTO answers SET ? `, setUp),
-  //   pool.query(`INSERT INTO photos (answer_id,url) VALUES (LAST_INSERT_ID(), SET ?)` , photos)
-  //  ])
-
-
 // this query promise does not need to return data
 AddAnAnswerModal = (setUp, photos)=> {
-  // let query1 =  pool.promise().query(`INSERT INTO answers SET ? `, setUp)
-
-  // let query2 =  pool.promise().query(`INSERT INTO photos (answer_id, url) VALUES(LAST_INSERT_ID(),'${photos}')` )
-  // let query2 = pool.promise().query(`SELECT LAST_INSERT_ID()`)
   return pool.promise().query(`INSERT INTO answers SET ? `, setUp).then( ()=> {
     pool.promise().query(`INSERT INTO photos (answer_id, url) VALUES(LAST_INSERT_ID(),'${photos}')` ).then(()=> {
-
     })
     .catch((err) =>{
       console.log(err)
@@ -101,81 +86,53 @@ AddAnAnswerModal = (setUp, photos)=> {
   })
 }
 
-// AddPhotosToAnswers = (photos) => {
-//   return new Promise((resolve,reject) => {
-//     pool.query(`INSERT INTO photos (answer_id,url) VALUES (LAST_INSERT_ID(), SET ?)` , photos, (error, asnwers) => {
-//       if(error) {
-//         return reject(error,'ERROR POSTING PHOTOS')
-//       }
-//       console.log(answers)
-//       return resolve('CREATED')
-//     })
-//   })
-// }
-
-
-
-
-
-
-
-
-
 
 // Updates a question to show it was found helpful.
-UpdateAnswerHelpful = () =>{
-  return new Promise((resolve, reject) => {
-    pool.query(`SELECT * FROM answers LIMIT 9`, (error, answers) => {
-      if(error) {
-        return reject(error)
-      }
-      return resolve(answers)
-    })
+UpdateQuestionHelpful = (question_id) =>{
+  return pool.promise().query(`UPDATE questions
+  SET questions.helpful=questions.helpful+1
+  WHERE questions.id= ${question_id};`)
+  .catch((err)=>{
+    console.log(err)
   })
 }
 
 // Updates a question to show it was reported. Note, this action does not delete the question, but the question will not be returned in the above GET request.
-UpdateReportQuestion = () =>{
-  return new Promise((resolve, reject) => {
-    pool.query(`SELECT * FROM questions LIMIT 9`, (error, questions) => {
-      if(error) {
-        return reject(error)
-      }
-      return resolve(questions)
-    })
+UpdateReportQuestion = (question_id) =>{
+  return pool.promise().query(`UPDATE questions
+  SET questions.reported= questions.reported+1
+  WHERE questions.id= ${question_id};`)
+  .catch((err)=>{
+    console.log(err)
   })
 }
 
-UpdateAnswerHelpful = () =>{
-  return new Promise((resolve, reject) => {
-    pool.query(`SELECT * FROM questions LIMIT 9`, (error, questions) => {
-      if(error) {
-        return reject(error)
-      }
-      return resolve(questions)
-    })
-  })
+// Updates answer helpful count
+UpdateAnswerHelpful = (answer_id) =>{
+ return pool.promise().query(`UPDATE answers
+ SET answers.helpful=answers.helpful+1
+ WHERE answers.id= ${answer_id}`)
+ .catch((err)=>{
+  console.log(err)
+ })
 }
 
-
-UpdateAnswerReport = () =>{
-  return new Promise((resolve, reject) => {
-    pool.query(`SELECT * FROM questions LIMIT 9`, (error, questions) => {
-      if(error) {
-        return reject(error)
-      }
-      return resolve(questions)
-    })
+// update answer rports, should not be included in get request for questions
+UpdateAnswerReport = (answer_id) =>{
+  return pool.promise().query(`UPDATE answers
+  SET answers.reported=answers.reported+1
+  WHERE answers.id = ${answer_id}`)
+  .catch((err)=>{
+   console.log(err)
   })
 }
 
 
-// organize order to match controllers -- api -learn
-  // is there anywhere else i can write the queries (save for refactor)
+module.exports = {SelectAllQuestions, SelectAllAnswers ,AddAQuestionModel, AddAnAnswerModal, UpdateAnswerHelpful,UpdateReportQuestion, UpdateQuestionHelpful, UpdateAnswerReport}
 
 
-// 3. create promise function for getting questions
-// 4. build query with controller info
-// 5. run/return query
-
-module.exports = {SelectAllQuestions, SelectAllAnswers ,AddAQuestionModel, AddAnAnswerModal, UpdateAnswerHelpful,UpdateReportQuestion}
+// still need to handle nulls
+// handle reported questions/answers to not be included in get
+// add index columns
+// add foreign keys
+// add loop to handle posting photos array
--- server/controllers.js
+++ server/controllers.js
@@ -3,7 +3,6 @@ const async =require('async');
 
   const getAllQuestions = (req, res) => {
     let product_id = req.query
-    console.log(product_id)
     db.SelectAllQuestions(product_id)
     .then((result) => {
       res.send(result)
@@ -32,9 +31,8 @@ const async =require('async');
       res.sendStatus(204)
     })
   }
-  // how to insert date on query generate date
+
  const addAQuestion = (req, res) => {
-  console.log(req.body)
     let body = req.body.body
     let asker_name = req.body.name
     let asker_email = req.body.email
@@ -42,7 +40,7 @@ const async =require('async');
     let setUp = {body, asker_name, asker_email,product_id}
     db.AddAQuestionModel(setUp)
     .then((result)=>{
-      res.send(201, result)
+      res.send(201)
     })
     .catch((error)=>{
       console.log(error)
@@ -72,19 +70,19 @@ const async =require('async');
  // Updates a question to show it was found helpful.
  const updateHelpfulQuestion = (req, res) => {
   let question_id = req.params.question_id
-  db.UpdateAnswerHelpful(question_id)
+  db.UpdateQuestionHelpful(question_id)
   .then((result) => {
-    res.sendStatus(204,'NO CONTENT')
+    res.send(204,'NO CONTENT')
   })
   .catch((error)=> {
-    res.status(500,'nope')
+    res.status(500,'[ ERROR UPDATING HELPFUL QUESTION ]')
   })
  }
 
 //  Updates a question to show it was reported. Note, this action does not delete the question, but the question will not be returned in the above GET request.
  const updateReportQuestion = (req, res) => {
-  let product_id = req.params.product_id
-  db.UpdateReportQuestion(product_id)
+  let question_id = req.params.question_id
+  db.UpdateReportQuestion(question_id)
   .then((result)=>{
     res.sendStatus(204, 'NO CONTENT')
   })
@@ -94,11 +92,11 @@ const async =require('async');
  }
 
 //  Updates an answer to show it was found helpful.
- const updateHelpfulAnswer = (req, resp) => {
+ const updateHelpfulAnswer = (req, res) => {
   let answer_id = req.params.answer_id
   db.UpdateAnswerHelpful(answer_id)
   .then((result)=>{
-   res.sendStatus(204)
+   res.send(204, 'NO CONTENT')
   })
   .catch((error)=>{
     res.status(500,'no in spanish')
@@ -106,23 +104,22 @@ const async =require('async');
  }
 
 // Updates an answer to show it has been reported
- const updateReportAnswer = (req, resp) => {
+ const updateReportAnswer = (req, res) => {
  let answer_id = req.params.answer_id
  db.UpdateAnswerReport(answer_id)
  .then((result) => {
-  res.sendStatus(204)
+  res.send(204,'NO CONTENT')
  })
  .catch((error)=> {
   res.status(500,'nada')
  })
 }
 
-// do i  need to check for duplicates?
-  // need to handle null returns to client
+
 
 module.exports = { getAllQuestions , getAllAnswers, addAQuestion, addAnAnswer,
   updateHelpfulQuestion, updateReportQuestion, updateHelpfulAnswer, updateReportAnswer
-
 };
 
 
+// fix resposnes to avoid depreciation message
